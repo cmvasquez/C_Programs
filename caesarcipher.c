bde785f2f352d24900e0bd54d02d292f0d99779e
Christian Vasquez <cvasquez3@luc.edu>
2022-04-01 17:57:30 +0000

    Caesar Cipher and other files from 4/1

--- caesarcipher.c
+++ caesarcipher.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+  char array[1024] = "this is a test string";
 
-char array[1024] = "attack";
+  void main() {
 
-void main() {
     int k;
     char *p; // pointer
 
     p = array;  // assigned to address that array lives at  
-    
+
     printf("array lives at %p\n", array);
 
     while(*p != '\0') {
+
         if(*p >= 'a' && *p <= 'z'){
-            *p -= 32; //ADD.B #2,(a0)
-        }
-        p++; // ADDA.L #1,A0
+
+            *p += 2; //ADD.B #2,(a0) // this moves the letter by 2 (A->C, F->H, etc)
+
+        }   
+
+        p++; // ADDA.L #1,A0 // increment the array
+
     }
+
     printf("%s\n", array);
     return;
-    }
+  }
